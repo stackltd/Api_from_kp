d06eb363ad5d963ac37e6afe59b7da2461914b87
Evgeny Fedorov <stackltd@yandex.ru>
2024-09-15 21:25:54 +0700

    Version 2.0 from 24.02.24 added web-site

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
+import re
+import sys
+
 from utils import Parser
 from deco import timer
 from messages import *
 
 parse = Parser()
 
+def zero():
+    parse.count = 0
+    parse.requests_all = 0
+    parse.count_dupl = 0
+    parse.stop = False
+    parse.start = True
+    del parse.dump_in
+
 @timer
 def main() -> None:
     """
     Функция позволяет заполнять базу данных в виде json информацией о фильмах из API Кинопоиска,
-    заполнять ее по известному id, а так же вывести информацию из json файла как в исходном виде, так и в кратком
+    заполнять ее по-известному id, а так же вывести информацию из json файла как в исходном виде, так и в кратком
     табличном формате.
     """
     while True:
@@ -17,27 +29,54 @@ def main() -> None:
         if ask_1 == '1':
             while True:
                 try:
-                    numb = int(input('Введите количество запросов к API\n'))
+                    numb = int(input('Введите количество записей, которые нужно добавить в базу\n'))
+                    if numb < 0:
+                        raise ValueError
                     ask_2 = input(ask_print)
                     break
-                except ValueError:
-                    print('Ошибка ввода')
-            [parse.make_json(prnt=ask_2 == '1') for _ in range(numb)]
+                except ValueError as ex:
+                    print('Ошибка ввода', file=sys.stderr)
+            print('in progress...')
+
+            while parse.count != numb and not parse.stop_parse:
+                try:
+                    if numb - parse.count <= 1:
+                        parse.stop = True
+                    parse.make_json(prnt=ask_2 == '1')
+                except Exception as ex:
+                    print(f'Произошла ошибка: {ex = }', file=sys.stderr)
+                    # raise
+                    # break
             if parse.count:
-                print(f'Количество записей в базе данных увеличено на {parse.count}\n')
-                parse.count = 0
+                print(f'Использовано запросов: {parse.requests_all}')
+                perc_dupl = round((parse.count_dupl / (parse.count_dupl + parse.count)) * 100)
+                print(f'Дублей: {parse.count_dupl} ({perc_dupl} %)')
+                zero()
             else:
                 print('База данных не была пополнена')
         elif ask_1 == '2':
             id_movies = []
-            id_mov = input('Введите название, по которому будет произовдиться поиск.\n'
-                           'Если нужно найти фильм по известному id, нашите звездочку после него\n')
+            id_mov = input('Введите название, по которому будет производиться поиск.\n'
+                           'Если нужно найти фильмы по известному id, составьте список id через пробел, с диапазоном'
+                           ' через дефис, и/или перечислением\n')
             ask_2 = input(ask_print)
-            if id_mov.endswith('*'):
-                id_movies.append(id_mov[:-1])
+            pattern = r'[^a-zA-Zа-яА-Я]+'
+            res = re.fullmatch(pattern, id_mov)
+            if res:
+                info = id_mov.split()
+                info = [[str(y) for y in range(int(x.split('-')[0]), int(x.split('-')[1]) + 1)]
+                        if '-' in x else x
+                        for x in info]
+                for x in info:
+                    if isinstance(x, list):
+                        id_movies.extend(x)
+                    else:
+                        id_movies.append(x)
             else:
                 id_movies.extend(parse.list_id(id_mov))
-            parse.make_json(id_movies=tuple(id_movies), prnt=ask_2=='1')
+            parse.stop =True
+            parse.make_json(id_movies=tuple(id_movies), prnt=ask_2 == '1', from_list_id=True)
+            zero()
         elif ask_1 == '3':
             ask_2 = input('Будет выведена таблица с краткой информацией о фильмах.'
                           ' Если же хотите вывести все данные, введите 1, если только таблицу - любой символ\n')
@@ -47,7 +86,8 @@ def main() -> None:
                 ask_3 = input('Введите для сортировки по возрастанию: 1: по имени, 2: по году, 3: по типу, 4: по жанру,'
                               ' 5: по голосам, 6: по стране. '
                               'В обратном порядке - введите двойное число'
-                              '\nДля поиска фильма и вывода в адаптированном виде введите id. В json-формате - напишите звездочку после id.'
+                              '\nДля поиска фильма и вывода в адаптированном виде введите id. В json-формате - '
+                              'напишите звездочку после id.'
                               '\nДля удаления фильма из базы введите его id и напишите знак минус (-) после него'
                               '\nВыход в основное меню - нажмите энтер.\n')
                 if ask_3 in ('1', '2', '3', '4', '5', '6', '11', '22', '33', '44', '55', '66'):
@@ -63,20 +103,31 @@ def main() -> None:
                         res = parse.get_json()
                         parse.print_table(obj=res)
                     else:
-                        obj = parse.get_json(prnt=False, id_movie=ask_3)
+                        obj = parse.get_json(id_movie=ask_3)
                         parse.print_info(obj=obj)
                         input('Нажмите ввод для вывода таблицы ')
                     parse.print_table(obj=res)
                 else:
                     break
         elif ask_1 == '4':
+            ask_2 = input('\nДля поиска фильма и вывода в адаптированном виде введите id. В json-формате - '
+                          'напишите звездочку после id.\n')
+            if ask_2.endswith('*'):
+                parse.get_json(prnt=True, id_movie=ask_2[:-1])
+            else:
+                obj = parse.get_json(id_movie=ask_2)
+                parse.print_info(obj=obj)
+        elif ask_1 == '5':
             ask_2 = input('Введите id фильма, который нужно удалить\n')
-            parse.del_movie(ask_2)
-            res = parse.get_json()
-            parse.print_table(obj=res)
+            res = parse.del_movie(ask_2)
+            if res:
+                print(f'Данные с id {ask_2} удалены')
+            else:
+                print(f'Данные с id {ask_2} не найдены')
         else:
             break
 
 
 if __name__ == '__main__':
+    print(os.getenv('PYTHONPATH'))
     main()
--- settings.py
+++ settings.py
@@ -1,1 +1,15 @@
-token = 'ВАШ-ТОКЕН'
+token = 'ВАШ ТОКЕН'
+
+
+# RANDOM_MOVIE = '/v1/movie/random'
+RANDOM_MOVIE = '/v1.4/movie/random'
+
+# примеры query параметров
+# query = {"notNullFields": "description"}
+# query = {"notNullFields": "description", "genres.name": "фантастика", "rating.kp": '3-10'}
+# query = {"notNullFields": "description", "genres.name": "ужасы", "rating.kp": '3-10'}
+# query = {"notNullFields": "description", "genres.name": ("фантастика", "триллер", "боевик", "комедия"), "rating.kp": '3-10'}
+query = {"notNullFields": "description", "genres.name": ("фантастика", "триллер", "боевик", "комедия"), "rating.kp": '3-10', "countries.name": ("США", "Великобритания")}
+# query= {"notNullFields": "description", "countries.name": "СССР"}
+# query = {"notNullFields": "description", "rating.kp": '3-10', "genres.name": '!мультфильм'}
+# query= {"notNullFields": "description", "countries.name": ("США", "Великобритания")}
